dd56aa6bc94e8f205f7e9e75d8906c9d7d949f5d
William Wallace <william@GamingNomads.com>
2018-12-03 05:02:23 -0500

    Rebuilt some of what I lost

--- NorthwindTraders/NorthwindTraders.Core/ApplicationService/ICustomerService.cs
+++ NorthwindTraders/NorthwindTraders.Core/ApplicationService/ICustomerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NorthwindTraders.Core.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,22 @@ namespace NorthwindTraders.Core.ApplicationService
 {
     public interface ICustomerService
     {
+        Customer NewCustomer(string CustomerID,
+                             string CompanyName,
+                             string ContactName,
+                             string ContactTitle,
+                             string Address,
+                             string City,
+                             string Region,
+                             string PostalCode,
+                             string Country,
+                             string Phone,
+                             string Fax);
+        Customer CreateCustomer(Customer cust);
+        Customer FindCustomerById(string customerId);
+        List<Customer> GetAllCustomers();
+        Customer UpdateCustomer(Customer customerUpdate);
+        Customer DeleteCustomer(string id);
+
     }
 }
---
+++ NorthwindTraders/NorthwindTraders.Core/ApplicationService/Services/CustomerService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NorthwindTraders.Core.DomainService;
+using NorthwindTraders.Core.Entity;
+
+namespace NorthwindTraders.Core.ApplicationService.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public Customer NewCustomer(string CustomerID, string CompanyName, string ContactName, string ContactTitle, string Address, string City, string Region, string PostalCode, string Country, string Phone, string Fax)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer CreateCustomer(Customer cust)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer FindCustomerById(string customerId)
+        {
+            return _customerRepository.ReadById(customerId);
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            return _customerRepository.ReadAll().ToList();
+        }
+
+        public Customer UpdateCustomer(Customer customerUpdate)
+        {
+            var customer = FindCustomerById(customerUpdate.CustomerID);
+            customer.Address = customerUpdate.Address;
+            customer.City = customerUpdate.City;
+            customer.CompanyName = customerUpdate.CompanyName;
+            customer.ContactName = customerUpdate.ContactName;
+            customer.ContactTitle = customerUpdate.ContactTitle;
+            customer.Country = customerUpdate.Country;
+            customer.CustomerID = customerUpdate.CustomerID;
+            customer.Fax = customerUpdate.Fax;
+            customer.Phone = customerUpdate.Phone;
+            customer.PostalCode = customerUpdate.PostalCode;
+            customer.Region = customerUpdate.Region;
+            return _customerRepository.Update(customer);
+        }
+
+        public Customer DeleteCustomer(string customerId)
+        {
+            return _customerRepository.Delete(customerId);
+        }
+    }
+}
